f9e306dee36c41302540ec278491ddf623c9f816
sricharanand <58458878+sricharanand@users.noreply.github.com>
2025-12-09 15:47:06 +0530

    Import ArrayList and List in AuthService
    Added imports for ArrayList and List in AuthService.

--- src/main/java/com/javaproject/java_project/service/AuthService.java
+++ src/main/java/com/javaproject/java_project/service/AuthService.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AuthService
 {
