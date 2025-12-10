431915395c94a8cc94fd38583ac072d7f5fa8eaa
sricharanand <58458878+sricharanand@users.noreply.github.com>
2025-12-10 16:17:45 +0530

    Refactor UsersRepository to return single User by username

--- src/main/java/com/javaproject/java_project/repository/UsersRepository.java
+++ src/main/java/com/javaproject/java_project/repository/UsersRepository.java
@@ -2,10 +2,8 @@ package com.javaproject.java_project.repository;
 import com.javaproject.java_project.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 
 public interface UsersRepository extends MongoRepository<User, Integer> {
-    List<User> findById(int id);
-    List<User> findByUsername(String username);
+    User findByUsername(String username);
 
 }
